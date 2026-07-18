a6dceb5570654f4ef64ea4bab44e7420f6410cc5
697-rgb <pushkargarg697@gmail.com>
2026-07-18 21:45:08 +0530

    Implement LCM algorithm with comprehensive tests

---
+++ mathforge/arithmetic/algorithms/lcm.py
@@ -0,0 +1,52 @@
+"""
+LEAST COMMON MULTIPLE
+
+this module provieds an implementation of the Least Common Multiple(LCD) algorithm using the GCD algo
+"""
+from mathforge.arithmetic.algorithms.gcd import gcd
+
+def lcm(a: int, b: int) ->int:
+    """
+    Compute the Least Common Multiple (LCM) of two integers.
+
+    The LCM is the smallest positive integer that is divisible
+    by both input integers.
+
+    Parameters
+    ----------
+    a : int
+        First integer.
+
+    b : int
+        Second integer.
+
+    Returns
+    -------
+    int
+        The least common multiple of the two integers.
+
+    Raises
+    ------
+    TypeError
+        If either argument is not an integer.
+
+    ValueError
+        If both arguments are zero.
+    """
+
+    # Reject booleans (bool is a subclass of int in Python)
+    if isinstance(a,bool) or isinstance(b,bool):
+        raise TypeError("Arguments must be integers, not booleans.")
+    
+    #validate type 
+    if not isinstance(a,int) or not isinstance(b,int):
+        raise TypeError()
+    #lcm(0,0) is undefined 
+    if a==0 and b==0:
+        raise ValueError("LCM is undefined for (0,0)")
+    
+    #by defination LCM(0,n)=0
+    if a==0 or b==0:
+        return 0
+    
+    return abs( (a//gcd(a,b))*b)
---
+++ mathforge/arithmetic/tests/test_lcm.py
@@ -0,0 +1,69 @@
+"""
+Unit tests for the LCM algo
+"""
+
+from mathforge.arithmetic.algorithms.lcm import lcm
+
+def run_tests()->None:
+    """Run all LCM test case"""
+
+    print("Running LCM test case")
+
+    #Normal case 
+
+    assert lcm(4, 6) == 12
+    assert lcm(6, 8) == 24
+    assert lcm(7, 13) == 91
+    assert lcm(5, 5) == 5
+
+    print("✓ Normal cases passed")
+
+    #Zero case
+
+    assert lcm(0, 5) == 0
+    assert lcm(5, 0) == 0
+
+    print("✓ Zero cases passed")
+
+    #Negative number 
+
+    assert lcm(-4, 6) == 12
+    assert lcm(4, -6) == 12
+    assert lcm(-4, -6) == 12
+
+    print("✓ Negative number cases passed")
+
+    # Invalid Types
+    invalid_inputs = [
+        (12.5, 5),
+        ("12", 5),
+        (True, 5),
+        ([], 5),
+        (None, 5),
+    ]
+
+    for a, b in invalid_inputs:
+        try:
+            lcm(a, b)
+        except TypeError:
+            pass
+        else:
+            raise AssertionError(f"Expected TypeError for gcd({a}, {b})")
+
+    print("✓ Invalid type cases passed")
+
+    # Undefined Case
+
+    try:
+        lcm(0, 0)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Expected ValueError for gcd(0, 0)")
+
+    print("✓ Undefined case passed")
+
+    print("\nAll LCM tests passed successfully! 🎉")
+
+if __name__ == "__main__":
+    run_tests()
